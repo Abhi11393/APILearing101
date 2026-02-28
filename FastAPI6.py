5e192890a7b248ee59350b852a2580f50b1f0956
Abhi11393 <abhijeet11393@gmail.com>
2026-02-28 21:10:35 +0530

    add FastAPI6 script and login template

---
+++ FastAPI6.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+async def hello_main():
+   return {"message":"Hello How are you?"}
+
+@app.get("/hello/{name}", response_class=HTMLResponse)
+async def hello(request: Request, name:str):
+   return templates.TemplateResponse("hello.html", {"request": request, "name":name})
+   
+@app.get("/login/", response_class=HTMLResponse)
+async def login(request: Request):
+   return templates.TemplateResponse("login.html", {"request": request})
